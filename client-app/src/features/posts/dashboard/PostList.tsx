47ce2e89fe1de97e5a8bddb9bcd42fbceaac697a
Wyatt Stevens <wyattstevens1995@gmail.com>
2024-02-29 11:38:41 -0700

    Added CRUD operations for client app.

---
+++ client-app/src/features/posts/dashboard/PostList.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Post } from "../../../app/models/post";
+import { Button, Item, Segment } from "semantic-ui-react";
+
+interface Props {
+  posts: Post[];
+  selectPost: (id: string) => void;
+  deletePost: (id: string) => void;
+}
+
+export default function PostList({ posts, selectPost, deletePost }: Props) {
+  return (
+    <Segment>
+      <Item.Group divided>
+        {posts.map((post) => (
+          <Item key={post.id}>
+            <Item.Content>
+              <Item.Header as="a">{post.title}</Item.Header>
+              <Item.Meta>{post.date}</Item.Meta>
+              <Item.Description>
+                <div>{post.description}</div>
+              </Item.Description>
+              <Item.Extra>
+                <Button onClick={() => selectPost(post.id)} floated="right" content="View" color="blue" />
+                <Button onClick={() => deletePost(post.id)} floated="right" content="Delete" color="red" />
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        ))}
+      </Item.Group>
+    </Segment>
+  );
+}
---
+++ client-app/src/features/posts/form/PostForm.tsx
@@ -0,0 +1,61 @@
+import React, { ChangeEvent, useState } from "react";
+import { Button, Form, Segment } from "semantic-ui-react";
+import { Post } from "../../../app/models/post";
+
+interface Props {
+  post: Post | undefined;
+  closeForm: () => void;
+  createOrEdit: (post: Post) => void;
+}
+
+export default function PostForm({ post: selectedPost, closeForm, createOrEdit }: Props) {
+  const initialState = selectedPost ?? {
+    id: "",
+    title: "",
+    description: "",
+    date: "",
+  };
+
+  const [post, setPost] = useState(initialState);
+
+  function handleSubmit() {
+    createOrEdit(post);
+  }
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    const { name, value } = event.target;
+    setPost({ ...post, [name]: value });
+  }
+
+  return (
+    <Segment clearing>
+      <Form onSubmit={handleSubmit} autoComplete="off">
+        <Form.Input
+          placeholder="Title"
+          value={post.title}
+          name="title"
+          onChange={handleInputChange}
+        />
+        <Form.TextArea
+          placeholder="Description"
+          value={post.description}
+          name="description"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="Date"
+          value={post.date}
+          name="date"
+          onChange={handleInputChange}
+        />
+        <Button floated="right" positive type="submit" content="Submit" />
+        <Button
+          onClick={closeForm}
+          floated="right"
+          type="button"
+          content="Cancel"
+        />
+      </Form>
+    </Segment>
+  );
+}
